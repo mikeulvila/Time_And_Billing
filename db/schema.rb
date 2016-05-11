a6df4ce970fcae910b8aa97ce83cfdc64281d619
Mike Ulvila <mikeulvila@me.com>
2016-05-11 13:37:29 -0500

    time_entry now inherits from account_entry

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160511154858) do
     t.integer  "employee_id", limit: 4
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "account_id",  limit: 4
   end
 
   create_table "accounts", force: :cascade do |t|
