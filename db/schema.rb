39e1d4c1df779ff8cc0f845e7164bfcc17fa7532
Mike Ulvila <mikeulvila@me.com>
2016-05-11 08:51:53 -0500

    generate model TimeEntry

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511040346) do
+ActiveRecord::Schema.define(version: 20160511134613) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20160511040346) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "email",      limit: 255
+  end
+
+  create_table "time_entries", force: :cascade do |t|
+    t.float    "time",        limit: 24
+    t.integer  "customer_id", limit: 4
+    t.integer  "employee_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
