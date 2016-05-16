f9d20d1f0703f11bc2433fa0a4c0e29f5afd7f0a
Mike Ulvila <mikeulvila@me.com>
2016-05-16 14:37:14 -0500

    validations on models

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,17 @@
 class Account < ActiveRecord::Base
   has_many :account_entries
+
+  validates :name, presence: true,
+                   length: {in: 1..70,
+                   message: "Please use a decent name."},
+                   uniqueness: true
+
+  validate :your_name_is_not_dumb
+
+  def your_name_is_not_dumb
+    if name.include?("dumb")
+      errors.add(:name, "is dumb")
+    end
+  end
+
 end
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -1,6 +1,7 @@
 class TimeEntry < AccountEntry
   belongs_to :employee
 
+  validates :time, numericality: {greater_than: 0, less_than: 24}
   def cost
     time * 34
   end
